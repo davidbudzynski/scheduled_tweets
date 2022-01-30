f820c49174857af8200cf7bacd65a5d7ed28901c
David Budzynski <david.budzynsky@gmail.com>
2022-01-30 19:01:18 +0000

    sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true,
+                    format: {
+                      with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+                      message: 'must be a valid email address'
+                    }
 end
