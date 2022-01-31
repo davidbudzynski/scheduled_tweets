b87b10ab69fa4c9cb0527621ad5296f0b2a6e32f
David Budzynski <david.budzynsky@gmail.com>
2022-01-31 22:05:34 +0000

    logging out users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@
 # root "articles#index"
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root to: 'main#index'
 end
